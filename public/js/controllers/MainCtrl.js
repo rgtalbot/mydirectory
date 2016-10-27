9293f9d0aa4619be3d18b22962a5ed54a49023a2
rgtalbot <rgtalbot@users.noreply.github.com>
2016-10-27 19:44:37 -0400

    logo added

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,21 +1,38 @@
-angular.module('MainCtrl', []).controller("MainController", ["$scope", "$http", function($scope, $http, $setValidity) {
+angular.module('MainCtrl', []).controller("MainController", ["$scope", "$http", function ($scope, $http, $setValidity) {
 
     $scope.tagline = 'To the moon and back!';
     console.log($scope.tagline);
 
 
-
-    $scope.urlValidate = function($setValidity) {
-        var organization = $scope.urlInput;
+    $scope.urlValidate = function ($setValidity) {
+        var organization = $scope.urlName;
 
         var queryString = "https://my-directory-api.herokuapp.com/api/v1/organizations/" + organization;
 
+
         $http.get(queryString).then(function successCallback(response) {
             console.log('not available');
-            $scope.signUpForm.urlInput.$setValidity("taken", false);
+            $scope.signUpForm.urlName.$setValidity("taken", false);
+            $scope.signUpForm.urlName.$setValidity("available", true);
         }, function errorCallback(response) {
             console.log('available');
+            $scope.signUpForm.urlName.$setValidity("available", false);
+            $scope.signUpForm.urlName.$setValidity("taken", true);
         })
 
+    };
+
+    $scope.companyChange = function () {
+        console.log('length', $scope.companyName.length);
+        var company = $scope.companyName;
+
+        if (company.length > 0) {
+            company.toLowerCase().replace(/\s/g, '-').replace(/[^a-z0-9-]/gi, "");
+            console.log(company);
+        }
+
+        $scope.urlName = company;
+        $scope.urlValidate($setValidity);
+
     }
-}])
+}]);
